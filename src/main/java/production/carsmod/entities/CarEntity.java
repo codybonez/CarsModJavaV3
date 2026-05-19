c9237fc94c9457f848a40a0feacc0375ec66f4c2
cody norwood <cody.norwood@student.centralia.edu>
2026-05-19 10:15:36 -0700

    Added interact function but no interaction

--- src/main/java/production/carsmod/entities/CarEntity.java
+++ src/main/java/production/carsmod/entities/CarEntity.java
@@ -1,16 +1,23 @@
 package production.carsmod.entities;
 
+import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ItemSteerable;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.VehicleEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.ValueInput;
 import net.minecraft.world.level.storage.ValueOutput;
 
-public class CarEntity extends VehicleEntity {
+public class CarEntity extends VehicleEntity  {
     public CarEntity(EntityType<? extends CarEntity> entityType, Level level) {
         super(entityType, level);
-        System.out.println("Spawn please");
+
     }
     public CarEntity(Level level){
         this(CarTypes.Car, level);
@@ -34,10 +41,27 @@ public class CarEntity extends VehicleEntity {
     @Override
     public void tick() {
         super.tick();
-//        System.out.println("Car is alive");
+
     }
 
-    //    public AttributeSupplier.Builder CreateAttributes() {
+
+
+    @Override
+    public InteractionResult interact(Player player, InteractionHand hand) {
+        InteractionResult superInteraction = super.interact(player, hand);
+        if (superInteraction != InteractionResult.PASS) {
+            System.out.println("Interaction failed");
+            return superInteraction;
+        } else {
+            System.out.println("Interaction is good");
+            return (player.isSecondaryUseActive()
+
+                    || !this.level().isClientSide() && !player.startRiding(this)
+                    ? InteractionResult.PASS
+                    : InteractionResult.SUCCESS);
+        }
+    }
+//    public AttributeSupplier.Builder CreateAttributes() {
 //    return
 //
 //    }
