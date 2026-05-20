3d5193cbcfbd0137bf37f9f700763891fd715a2b
cody norwood <cody.norwood@student.centralia.edu>
2026-05-20 10:58:05 -0700

    Added smaller hitbox to car

--- src/main/java/production/carsmod/entities/CarEntity.java
+++ src/main/java/production/carsmod/entities/CarEntity.java
@@ -1,22 +1,36 @@
 package production.carsmod.entities;
 
+import com.google.common.collect.Lists;
 import net.minecraft.core.BlockPos;
 import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ItemSteerable;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.vehicle.DismountHelper;
 import net.minecraft.world.entity.vehicle.VehicleEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.WaterlilyBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.ValueInput;
 import net.minecraft.world.level.storage.ValueOutput;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import java.util.List;
 
 public class CarEntity extends VehicleEntity  {
+
+
+
+
+
     public CarEntity(EntityType<? extends CarEntity> entityType, Level level) {
         super(entityType, level);
 
@@ -44,7 +58,16 @@ public class CarEntity extends VehicleEntity  {
     public void tick() {
         super.tick();
 
+        this.move(MoverType.SELF, this.getDeltaMovement());
     }
+
+
+    @Override
+    public void move(MoverType moverType, Vec3 vec3) {
+        super.move(moverType, vec3);
+    }
+
+
 
 
 
