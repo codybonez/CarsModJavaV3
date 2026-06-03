5142fdd97bd2b740502401e5173fea59d4df37e5
cody norwood <cody.norwood@student.centralia.edu>
2026-06-03 12:04:27 -0700

    Need to make sure it moves correctly

--- src/main/java/production/carsmod/entities/CarEntity.java
+++ src/main/java/production/carsmod/entities/CarEntity.java
@@ -22,26 +22,28 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import production.carsmod.items.CarItem;
 
 import java.util.List;
 
 public class CarEntity extends VehicleEntity  {
 
 
-
-
+    private boolean inputLeft;
+    private boolean inputRight;
+    private boolean inputUp;
+    private boolean inputDown;
+    private float deltaRotation;
 
     public CarEntity(EntityType<? extends CarEntity> entityType, Level level) {
         super(entityType, level);
 
     }
-    public CarEntity(Level level){
-        this(CarTypes.Car, level);
-    }
+
 
     @Override
     protected Item getDropItem() {
-        return null;
+        return CarItem.CAR_SPAWN_EGG;
     }
 
     @Override
@@ -57,13 +59,58 @@ public class CarEntity extends VehicleEntity  {
     @Override
     public void tick() {
         super.tick();
-
         this.move(MoverType.SELF, this.getDeltaMovement());
+        if (this.level().isClientSide()){
+        this.controlBoat();
+     }
+//        this.move(MoverType.SELF, this.getDeltaMovement());
     }
 
+    private void controlBoat() {
+        if (this.isVehicle()) {
+            float f = 0.0F;
+            if (this.inputLeft) {
+                this.deltaRotation--;
+            }
+
+            if (this.inputRight) {
+                this.deltaRotation++;
+            }
+
+            if (this.inputRight != this.inputLeft && !this.inputUp && !this.inputDown) {
+                f += 0.005F;
+            }
+
+            this.setYRot(this.getYRot() + this.deltaRotation);
+
+            if (this.inputUp) {
+                f += 0.04F;
+            }
+
+            if (this.inputDown) {
+                f -= 0.005F;
+            }
+
+            this.setDeltaMovement(
+                    this.getDeltaMovement().add(Mth.sin(-this.getYRot() * (float) (Math.PI / 180.0)) * f, 0.0, Mth.cos(this.getYRot() * (float) (Math.PI / 180.0)) * f)
+            );
+
+        }
+    }
+    public void setInput(boolean bl, boolean bl2, boolean bl3, boolean bl4) {
+        this.inputLeft = bl;
+        this.inputRight = bl2;
+        this.inputUp = bl3;
+        this.inputDown = bl4;
+    }
+    @Override
+    public boolean startRiding(Entity entity, boolean bl, boolean bl2) {
+        return true;
+    }
 
     @Override
     public void move(MoverType moverType, Vec3 vec3) {
+
         super.move(moverType, vec3);
     }
 
