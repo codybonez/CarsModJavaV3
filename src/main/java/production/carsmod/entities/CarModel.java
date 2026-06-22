f765e0057312fd2888de0ce945f8793cace20011
Cody <CodyBonez@yahoo.com>
2026-06-22 16:03:35 -0700

    Fixed the textures with EntityRenderer but it might cause problems in the future (Go back to this commit if having issues)

--- src/main/java/production/carsmod/entities/CarModel.java
+++ src/main/java/production/carsmod/entities/CarModel.java
@@ -8,6 +8,7 @@ import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.world.entity.animal.Animal;
 import org.intellij.lang.annotations.Identifier;
 
@@ -42,6 +43,20 @@ public class CarModel extends EntityModel<CarRenderState> {
 				.texOffs(0, 50).addBox(-9.7F, -6.6F, -12.7F, 20.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
 				.texOffs(36, 53).addBox(5.1F, -4.9F, 12.0F, 2.0F, 2.0F, 0.0F, new CubeDeformation(0.0F))
 				.texOffs(40, 53).addBox(-7.1F, -4.9F, 12.0F, 2.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.1F, 20.9F, 0.0F, 0.0F, 3.1416F, 0.0F));
+
+
+		// Non flipped model
+//		PartDefinition Wheels = partdefinition.addOrReplaceChild("Wheels", CubeListBuilder.create().texOffs(44, 50).addBox(6.3F, -1.0F, -9.0F, 2.0F, 4.0F, 4.0F, new CubeDeformation(0.0F))
+//				.texOffs(0, 53).addBox(-8.8F, -1.0F, -9.0F, 2.0F, 4.0F, 4.0F, new CubeDeformation(0.0F))
+//				.texOffs(12, 53).addBox(6.2F, -1.0F, 5.0F, 2.0F, 4.0F, 4.0F, new CubeDeformation(0.0F))
+//				.texOffs(24, 53).addBox(-8.8F, -1.0F, 5.0F, 2.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-0.2F, 0.8F, 0.0F));
+//
+//		PartDefinition Body = partdefinition.addOrReplaceChild("Body", CubeListBuilder.create().texOffs(0, 27).addBox(-7.0F, 0.5F, -11.0F, 14.0F, 1.0F, 22.0F, new CubeDeformation(0.0F))
+//				.texOffs(0, 0).addBox(-8.0F, 1.6F, -11.1F, 16.0F, 4.0F, 23.0F, new CubeDeformation(0.0F))
+//				.texOffs(0, 50).addBox(-9.7F, 5.6F, -12.7F, 20.0F, 1.0F, 2.0F, new CubeDeformation(0.0F))
+//				.texOffs(36, 53).addBox(5.1F, 2.9F, 12.0F, 2.0F, 2.0F, 0.0F, new CubeDeformation(0.0F))
+//				.texOffs(40, 53).addBox(-7.1F, 2.9F, 12.0F, 2.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-0.1F, 3.1F, 0.0F, 0.0F, 3.1416F, 0.0F));
+
 
 		return LayerDefinition.create(meshdefinition, 128, 128);
 
--- src/main/java/production/carsmod/entities/CarRenderer.java
+++ src/main/java/production/carsmod/entities/CarRenderer.java
@@ -1,17 +1,24 @@
 package production.carsmod.entities;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.state.BoatRenderState;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
 import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.state.CameraRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.vehicle.boat.AbstractBoat;
@@ -27,62 +34,60 @@ import production.carsmod.CarsMod;
 import production.carsmod.CarsModModelLayers;
 
 
-public class CarRenderer extends MobRenderer<CarEntity, CarRenderState, CarModel> {
+public class CarRenderer extends EntityRenderer<CarEntity, CarRenderState> {
 
     private  final Identifier texture = Identifier.fromNamespaceAndPath(CarsMod.MOD_ID, "/textures/entity/car_texture_main.png");
 
-//    public final RenderType renderType;
-
+    public final RenderType renderType;
+    public CarModel model;
     public CarRenderer(EntityRendererProvider.Context context) {
-        super(context, new CarModel(context.bakeLayer(CarsModModelLayers.CAR)), 0.4f);
+        super(context);
+        this.model = new CarModel(context.bakeLayer(CarsModModelLayers.CAR));
+     renderType =  model.renderType(texture);
 
 
-//        this.renderType = this.model.renderType(this.texture);
-//
         CarsMod.LOGGER.info("Car rendered");
 
     }
 
-//    @Override
-//
-//    public void submit(CarRenderState carRenderState, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, CameraRenderState cameraRenderState) {
-//        super.submit(carRenderState, poseStack, submitNodeCollector, cameraRenderState);
-//        poseStack.pushPose();
-//
-//
-//        poseStack.translate(0.0F, 0.0f, 0.0F);
-//
-////        poseStack.mulPose(Axis.YP.rotationDegrees(360.0F - carRenderState.yRot));
-//        poseStack.mulPose(new Quaternionf().setAngleAxis(carRenderState.yRot * (float) (Math.PI / 180.0), 0.0F, -1.0F, 0.0F));
-//
-//
-//
-//
-//        submitNodeCollector.submitModel(
-//                this.model, carRenderState, poseStack, this.renderType(), carRenderState.lightCoords, OverlayTexture.NO_OVERLAY, carRenderState.outlineColor, null
-//        );
-//
-//        poseStack.popPose();
-//
-//    }
 
-    @Override
-    public Identifier getTextureLocation(CarRenderState livingEntityRenderState) {
-        return texture;
+        @Override
+    public void submit(CarRenderState carRenderState, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, CameraRenderState cameraRenderState) {
+        super.submit(carRenderState, poseStack, submitNodeCollector, cameraRenderState);
+        poseStack.pushPose();
+
+
+        poseStack.translate(0.0F, 1.5f, 0.0F);
+
+        poseStack.mulPose(Axis.YP.rotationDegrees(360.0F - carRenderState.yRot));
+
+        // Keep this or car will flip
+        poseStack.scale(1.0f, -1.0f, -1.0f);
+
+
+        submitNodeCollector.submitModel(
+                model(), carRenderState, poseStack, renderType,
+                carRenderState.lightCoords, OverlayTexture.NO_OVERLAY, carRenderState.outlineColor
+                , null
+        );
+
+        poseStack.popPose();
+
     }
+
+
 
     @Override
     public CarRenderState createRenderState() {
         return new CarRenderState();
     }
 
-    protected EntityModel<CarRenderState> model() {
-        return this.model;
+    protected Model<CarRenderState> model() {
+
+        return model;
     }
 
-    protected RenderType renderType() {
-        return this.model.renderType(this.texture);
-    }
+
 
     public void extractRenderState(CarEntity entity, CarRenderState renderState, float f) {
         super.extractRenderState(entity, renderState, f);
